56eab921225deeb4b476aa36f326a7c1a5b6a648
SamuelSherpa2020 <64248032+SamuelSherpa2020@users.noreply.github.com>
2020-07-16 12:45:03 +0545

    Add files via upload

---
+++ grandpa.java
@@ -0,0 +1,37 @@
+public class grandpa {
+    public void bindas(){
+        System.out.println("Usually grandpa used to live without any fear");
+    }
+    public class father extends grandpa{
+        public void profession(){
+            System.out.println("My father's profession is church ministry");
+        }
+    }
+    public class samuel extends father{
+        public void coding(){
+            System.out.println("I, Samuel love coding");
+        }
+    }
+    public class mom extends grandpa[
+        public void cooking(){
+            System.out.println("My mom cooks food very nicely");
+        }
+    ]
+    public class susu extends mom{
+        public void subuhabit(){}
+        System.out.println("subu has also started to cook food");
+    }
+}
+public class hierarchicalinheritance{
+    public static void main(String[] args) {
+        father f = new father();
+        b.bindas();
+        b.profession();
+         mom haha = new mom();
+         haha.bindas();
+         susu rame = new susu();
+         rame.bindas();
+         rame.haha(); 
+    }
+}
+}
